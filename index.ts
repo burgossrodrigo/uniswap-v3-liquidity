472cbac8a12d7ee185c58a372cde323daaadf919
Rodrigo Burgos <rodrigoburgos@live.com>
2024-05-14 15:25:07 -0300

    fix: price range calc

--- index.ts
+++ index.ts
@@ -2,10 +2,7 @@ import { ethers, JsonRpcProvider } from 'ethers'
 import { ERC20__factory, NFPM__factory, V3Factory__factory, V3Pool__factory } from './types/ethers-contracts'
 require('dotenv').config()
 
-const TICK_BASE = 1.0001;
 const PRICE_RANGE_PERCENT = 0.05
-const AMOUNT = 300
-const tickSpacing = 60;
 
 const main = async () => {
     try {
@@ -24,24 +21,10 @@ const main = async () => {
     
         const [ decimalsA, decimalsB ] = await Promise.all([TOKEN_A.decimals(), TOKEN_B.decimals()])
 
-        // const mintTokenA = await TOKEN_A.mint(wallet.address, String(formatAmount(AMOUNT, Number(decimalsA))))
-        // const mintTokenB = await TOKEN_B.mint(wallet.address, String(formatAmount(AMOUNT, Number(decimalsB))))
-
-        // await mintTokenA.wait()
-        // await mintTokenB.wait()
-
         const balanceA = await TOKEN_A.balanceOf(wallet.address)
         const balanceB = await TOKEN_B.balanceOf(wallet.address)
     
         console.log(balanceA.toString(), balanceB.toString(), 'balances')
-        // const formatedAmmountA = formatAmount(Number(formatAmountFrom(Number(balanceA), Number(decimalsA))), Number(decimalsA))
-        // const formatedAmmountB = formatAmount(Number(formatAmountFrom(Number(balanceA), Number(decimalsA))), Number(decimalsB))
-    
-        // const sqrtPrice = priceToSqrtPrice(1, Number(decimalsA), Number(decimalsB))
-    
-        // const initializePoolTx = await V3Pool.initialize(BigInt(sqrtPrice))
-    
-        // const initializePoolReceipt = await initializePoolTx.wait()
     
         const slot0 = await V3Pool.slot0()
         const token0 = await V3Pool.token0()
@@ -53,12 +36,6 @@ const main = async () => {
     
         const tickLower = calculateTickRange(price, PRICE_RANGE_PERCENT, Number(tickSpacing)).tickLower
         const tickerUpper = calculateTickRange(price, PRICE_RANGE_PERCENT, Number(tickSpacing)).tickUpper
-    
-        // const approveATx = await TOKEN_A.approve(process.env.NFPM as string, String(balanceA))
-        // const approveBTx = await TOKEN_B.approve(process.env.NFPM as string, String(balanceB))
-    
-        // const approveAReceipt = await approveATx.wait()
-        // const approveBReceipt = await approveBTx.wait()
     
         const mintParams = {
             nonFungibleAddress: process.env.NFPM as string,
@@ -84,11 +61,7 @@ const main = async () => {
     
         const mintReceipt = await mintTx.wait()
     
-        return { 
-            "mintTx": mintReceipt, 
-            // "approveAReceipt": approveAReceipt, 
-            // "approveBReceipt": approveBReceipt
-        } 
+        return { "mintTx": mintReceipt } 
 
     } catch (error: Error | any) {
         throw new Error(error.message)
@@ -116,19 +89,19 @@ function calculateTickRange(price: number, percentage: number, tickSpacing: numb
     return { tickLower, tickUpper };
 }
 
-function priceToTick(price: number, tickSpacing: number): number {
+function priceToTick(price: number): number {
     const logBase = Math.log(1.0001);
-    return Math.floor(Math.log(price) / logBase / tickSpacing) * tickSpacing;
+    return Math.floor(Math.log(price) / logBase);
 }
 
 function calculateTickUpper(price: number, tickSpacing: number): number {
-    const tick = priceToTick(price, tickSpacing);
-    return tick + (tickSpacing - (tick % tickSpacing));
+    return Math.ceil(priceToTick(price) / tickSpacing) * tickSpacing;
 }
 
 function calculateTickLower(price: number, tickSpacing: number): number {
-    return priceToTick(price, tickSpacing);
+    return Math.floor(priceToTick(price) / tickSpacing) * tickSpacing;
 }
+
 
 const formatAmount = (amount: number, decimals: number): BigInt => {
     return BigInt(amount * Math.pow(10, decimals))
